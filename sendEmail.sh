19e6d5cc392fa6e42d8fb3a130de50c9be034695
rockeek <rockeek@yahoo.com>
2017-06-29 18:22:58 +0000

    Dry run possible. Keyboards us, fr, ch-fr

---
+++ sendEmail.sh
@@ -0,0 +1,245 @@
+#####dry mode#####
+usleep 3000000
+usleep 0
+usleep 750000
+usleep 0
+echo "left-meta r" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+usleep 0
+usleep 1000000
+usleep 0
+echo "p" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "o" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "z" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "e" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "r" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "s" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "h" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "e" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "l" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "l" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+usleep 0
+echo "enter" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+usleep 0
+usleep 750000
+usleep 0
+echo "rbracket" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "left-shift o" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "u" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "t" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "l" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "o" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "o" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "k" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "space" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "equal" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "space" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "left-shift n" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "e" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "z" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "6" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "left-shift o" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "b" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "j" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "e" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "c" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "t" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "space" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "6" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "left-shift c" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "o" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "semicolon" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "left-shift o" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "b" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "j" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "e" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "c" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "t" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "space" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "left-shift o" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "u" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "t" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "l" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "o" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "o" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "k" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "left-shift comma" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "left-shift q" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "p" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "p" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "l" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "i" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "c" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "q" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "t" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "i" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "o" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "n" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+usleep 0
+echo "enter" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+usleep 0
+usleep 1500000
+usleep 0
+echo "rbracket" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "left-shift semicolon" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "q" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "i" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "l" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "space" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "equal" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "space" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "rbracket" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "left-shift o" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "u" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "t" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "l" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "o" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "o" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "k" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "left-shift comma" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "left-shift c" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "r" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "e" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "q" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "t" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "e" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "left-shift i" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "t" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "e" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "semicolon" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "5" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "0" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "minus" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+usleep 0
+echo "enter" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+usleep 0
+usleep 1500000
+usleep 0
+echo "rbracket" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "left-shift semicolon" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "q" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "i" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "l" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "left-shift comma" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "left-shift t" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "o" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "space" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "equal" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "space" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "3" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "r" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "c" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "h" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "q" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "t" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "t" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "i" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "right-alt 0" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "j" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "z" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "left-shift comma" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "o" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "r" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "g" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "3" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+usleep 0
+echo "enter" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+usleep 0
+usleep 1500000
+usleep 0
+echo "rbracket" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "left-shift semicolon" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "q" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "i" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "l" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "left-shift comma" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "left-shift s" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "u" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "b" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "j" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "e" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "c" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "t" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "space" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "equal" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "space" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "3" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "left-shift s" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "u" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "r" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "p" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "r" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "i" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "s" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "e" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "space" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "s" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "u" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "r" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "p" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "r" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "e" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "n" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "q" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "n" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "t" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "e" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "3" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+usleep 0
+echo "enter" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+usleep 0
+usleep 1500000
+usleep 0
+echo "rbracket" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "left-shift semicolon" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "q" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "i" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "l" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "left-shift comma" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "left-shift b" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "o" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "d" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "y" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "space" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "equal" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "3" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "left-shift s" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "n" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "i" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "c" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "k" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "e" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "r" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "s" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "space" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "p" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "o" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "u" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "r" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "space" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "left-shift f" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "l" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "o" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "r" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "i" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "q" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "n" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "slash" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "3" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+usleep 0
+echo "enter" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+usleep 0
+usleep 1500000
+usleep 0
+echo "rbracket" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "left-shift semicolon" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "q" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "i" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "l" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "left-shift comma" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "left-shift s" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "e" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "n" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "d" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "5" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+echo "minus" | ./hid-gadget-test /dev/hidg0 keyboard > /dev/null
+usleep 0
